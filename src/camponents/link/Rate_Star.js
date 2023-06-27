7994ef93a2b495913286eb3c6b7e4751eb762fda
Urvishsakhareliya <106795173+Urvishsakhareliya@users.noreply.github.com>
2023-06-27 11:55:47 +0530

    rateStar

---
+++ src/camponents/link/Rate_Star.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Rate_Star({ stars, reviews }) {
+  return <div></div>;
+}
--- src/camponents/link/Services.js
+++ src/camponents/link/Services.js
@@ -3,6 +3,8 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { context } from "../../Context";
 import Image from "./Image";
+import Rate_Star from "./Rate_Star";
+import Color from "./Color";
 
 export default function Service() {
   const Api = "https://api.pujakaitem.com/api/products";
@@ -48,9 +50,13 @@ export default function Service() {
 
               <div className="col-6">
                 <h4>{name}</h4>
+                <Rate_Star abc={(stars, reviews)} />
                 <p>
                   <b>Company:</b> {company}
                 </p>
+
+                <p className="mt-2">{description}</p>
+                <Color color={color} />
               </div>
             </div>
           </section>
