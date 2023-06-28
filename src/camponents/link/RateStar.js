f965bf95ff46b92ed190018959e6f880ac4824dc
Urvishsakhareliya <106795173+Urvishsakhareliya@users.noreply.github.com>
2023-06-28 09:37:03 +0530

    28/06

--- src/camponents/link/RateStar.js
+++ src/camponents/link/RateStar.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { BsStarFill, BsStar } from "react-icons/bs";
+import { BsStarFill, BsStar, BsStarHalf } from "react-icons/bs";
 
 export default function RateStar({ star, reviews }) {
-  // console.log();
-  const fillrate = (val) => {
-    return (val * 100) / 5;
-  };
-  console.log(`${fillrate(star)}%`);
+  const RatingStar = Array.from({ length: 5 }, (ele, index) => {
+    let number = index + 0.5;
+    return (
+      <span key={index}>
+        {star >= index + 1 ? (
+          <BsStarFill />
+        ) : star >= number ? (
+          <BsStarHalf />
+        ) : (
+          <BsStar />
+        )}
+      </span>
+    );
+  });
+
+  // const fillrate = (val) => {
+  //   return (val * 100) / 5;
+  // };
 
   return (
     <>
       <div className="star position-relative d-inline-block">
-        <div className="borderStar">
+        {/*<div className="borderStar">
           <div className="group d-flex">
             <BsStar />
             <BsStar />
@@ -28,7 +41,10 @@ export default function RateStar({ star, reviews }) {
             <BsStarFill />
             <BsStarFill />
           </div>
-        </div>
+        </div> */}
+
+        {RatingStar}
+        <p>({reviews} Customer Reviews)</p>
       </div>
     </>
   );
